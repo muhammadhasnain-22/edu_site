191502dd660d6661d7e23f29299f9a554567caad
muhammadhasnain-22 <mhasnainmaqsood8452412@gmail.com>
2024-10-06 00:22:33 +0500

    nav, home and about.css files are modified

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
+
+let nav = document.querySelector(".nav"),
+searchIcon = document.querySelector("#searchIcon");
+openBtn = document.querySelector('.openBtn') 
+closeBtn = document.querySelector('.closeBtn') 
+
+
 // for search box  
-
-const nav = document.querySelector(".nav"),
-    searchIcon = document.querySelector("#searchIcon");
-
-    searchIcon.addEventListener("click", () => {
+searchIcon.addEventListener("click", () => {
     nav.classList.toggle("openSearch");
 });
+
+
+
+// for nav links ,
+openBtn.addEventListener("click", () => {
+    nav.classList.add('openLinks');
+});
+closeBtn.addEventListener("click", () => {
+    nav.classList.remove('openLinks');
+});
+
+
+
+
+
